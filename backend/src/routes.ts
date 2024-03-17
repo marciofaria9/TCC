00a2c50412030ef2c6310be36da05934b2f908b2
Marcio Faria <m.faria.jr@icloud.com>
2024-03-17 18:16:33 -0300

    product

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,22 +1,37 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+import multer from 'multer';
 
-import { AuthUserController } from './controllers/user/AuthUserController';
-import { CreateUserController } from './controllers/user/CreateUserController';
-import { DetailUserController } from './controllers/user/DetailUserController';
-import { isAuthenticated } from './middlewares/isAuthenticated';
-import { CreateCategoryController } from './controllers/category/CreateCategoryController';
-import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
+
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
+
+import { CreateProductController } from './controllers/product/CreateProductController'
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
+import uploadConfig from './config/multer'
 
 const router = Router();
 
-//user
-router.post('/users', new CreateUserController().handle)
-router.post('/session', new AuthUserController().handle)
-router.get('/me', isAuthenticated, new DetailUserController().handle)
+const upload = multer(uploadConfig.upload("./tmp"));
 
-//category
-router.post('/category', isAuthenticated, new CreateCategoryController().handle)
-router.get('/category', isAuthenticated, new ListCategoryController().handle)
+//-- ROTAS USER --
+router.post('/users', new CreateUserController().handle)
+
+router.post('/session', new AuthUserController().handle)
+
+router.get('/me', isAuthenticated,  new DetailUserController().handle )
+
+//-- ROTAS CATEGORY
+router.post('/category', isAuthenticated, new CreateCategoryController().handle )
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle )
+
+//-- ROTAS PRODUCT
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle )
 
 
 export { router }; 
