22c2e2f58505ff3863532ebfe35b9d1b45a427e0
Marcio Faria <m.faria.jr@icloud.com>
2024-03-21 20:21:25 -0300

    order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -8,6 +8,8 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from './controllers/category/ListCategoryController'
 import { CreateProductController } from './controllers/product/CreateProductController'
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
+import { CreateOrderController } from './controllers/Order/CreateOrderController';
+import { RemoveOrderController } from './controllers/order/RemoveOrderController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -29,5 +31,9 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle )
 //PRODUCT
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle )
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle )
+
+//ORDER
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export { router }; 
