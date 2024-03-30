b74b4420392c7cfae391f2d825d22aba613e0651
Marcio Faria <m.faria.jr@icloud.com>
2024-03-30 10:33:17 -0300

    frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    < div className={styles.container}>
+      <h1>Pizzaria</h1>
+    </div>
+  )
+}
