5e220317f105efd87c1228993c010fda8043a023
Marcio Faria <m.faria.jr@icloud.com>
2024-04-09 21:07:04 -0300

    inicio tela login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,33 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/home.module.scss';
+
+import logoImg from '../../public/logo.svg';
+import { Input} from '../components/ui/Input'
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Pizzaria - Login</title> 
-    </Head>
-    <div>
-      <h1>Pizzaria</h1>
-    </div>
+      <Head>
+        <title>Pizzaria - Login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Pizzaria" />
+
+        <div className={styles.login}></div>
+        <form>
+          <Input
+          placeholder='Digite seu email'
+          type="text"
+          />
+           <Input
+          placeholder='Digite sua senha'
+          type="password"
+          />
+        </form>
+
+      </div>
+
     </>
   )
 }
