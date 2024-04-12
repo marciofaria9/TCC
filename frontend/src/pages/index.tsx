687a07c77968f4830fa2fd4d71bba54e8a0a8b72
Marcio Faria <m.faria.jr@icloud.com>
2024-04-11 21:51:41 -0300

    pagina login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,31 +3,42 @@ import Image from 'next/image';
 import styles from '../styles/home.module.scss';
 
 import logoImg from '../../public/logo.svg';
-import { Input} from '../components/ui/Input'
+import { Input } from '../components/ui/Input'
+import { Button } from '../components/ui/Button';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Pizzaria - Login</title>
-      </Head>
-      <div className={styles.containerCenter}>
-        <Image src={logoImg} alt="Logo Pizzaria" />
+    <Head>
+      <title>Pizzaria - Faça seu login</title> 
+    </Head>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo Pizzaria" />
 
-        <div className={styles.login}></div>
+      <div className={styles.login}>
         <form>
           <Input
-          placeholder='Digite seu email'
-          type="text"
+            placeholder="Digite seu email"
+            type="text"
           />
-           <Input
-          placeholder='Digite sua senha'
-          type="password"
+
+          <Input
+            placeholder="Sua senha"
+            type="password"
           />
+          
+          <Button
+            type="submit"
+            loading={false}
+          >
+            Cadastrar
+          </Button>
+
         </form>
+       <a className={styles.text}>Não possui uma conta? Cadastrar</a>
 
       </div>
-
+    </div>
     </>
   )
 }
