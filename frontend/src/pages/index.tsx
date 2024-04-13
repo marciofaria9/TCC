ef4fd2619f97fed4fbb4ae95b11fe2381eb5ad73
Marcio Faria <m.faria.jr@icloud.com>
2024-04-13 10:39:10 -0300

    cadastro user

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useContext, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/home.module.scss';
@@ -5,40 +6,69 @@ import styles from '../styles/home.module.scss';
 import logoImg from '../../public/logo.svg';
 import { Input } from '../components/ui/Input'
 import { Button } from '../components/ui/Button';
+import { AuthContext } from '@/contexts/AuthContext';
+
+import Link from 'next/link';
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loading, setLoading] = useState(false)
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+
+  }
+
   return (
     <>
-    <Head>
-      <title>Pizzaria - Faça seu login</title> 
-    </Head>
-    <div className={styles.containerCenter}>
-      <Image src={logoImg} alt="Logo Pizzaria" />
+      <Head>
+        <title>Pizzaria - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Pizzaria" />
 
-      <div className={styles.login}>
-        <form>
-          <Input
-            placeholder="Digite seu email"
-            type="text"
-          />
+        <div className={styles.login}>
+          <form onSubmit={handleLogin}>
+            <Input
+              placeholder="Digite seu email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-          <Input
-            placeholder="Sua senha"
-            type="password"
-          />
-          
-          <Button
-            type="submit"
-            loading={false}
-          >
-            Cadastrar
-          </Button>
+            <Input
+              placeholder="Sua senha"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-        </form>
-       <a className={styles.text}>Não possui uma conta? Cadastrar</a>
+            <Button
+              type="submit"
+              loading={false}
+            >
+              Acessar
+            </Button>
+          </form>
 
+          <Link href="/signup">
+            <span className={styles.text}>Ja possui uma conta? Faça login</span>
+          </Link>
+
+
+        </div>
       </div>
-    </div>
     </>
   )
 }
