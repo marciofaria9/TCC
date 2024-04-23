738034473cf8a5ecfd34a8c2a9ac2c1bf5e24782
Marcio Faria <m.faria.jr@icloud.com>
2024-04-22 21:01:16 -0300

    cadastro categoria

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,10 +1,21 @@
 import { canSSRAuth } from '../../utils/canSSRAuth'
+import Head from 'next/head'
+import { Header } from '../../components/Header'
 
 export default function Dashboard() {
     return (
-        <div>
-            <h1>Bem vindo</h1>
-        </div>
+        <>
+            <Head>
+                <title> Home Pizzaria</title>
+            </Head>
+            <div>
+            <Header/>
+
+            
+                <h1>Painel</h1>
+            </div>
+
+        </>
     )
 }
 
